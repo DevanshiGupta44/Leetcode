98de478e0373fa9b694b6b19d42d5279444742c8
Devanshi Gupta <121246424+DevanshiGupta44@users.noreply.github.com>
2024-04-23 13:56:35 +0530

    Time: 74 ms (96.90%), Space: 60.9 MB (73.88%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        // sort(nums.begin(), nums.end());
-        // int n = nums.size();
-        // for (int i = 1; i < n; i++) {
-        //     if (nums[i] == nums[i - 1])
-        //         return true;
-        // }
-        // return false;
-        //code
         int n = nums.size();
         sort(nums.begin(),nums.end());
         for(int i=1;i<n;i++){
@@ -19,30 +11,3 @@ public:
         return false;
     }
 };
-// class Solution {
-// public:
-//     bool containsDuplicate(vector<int>& nums) {
-//         int n = nums.size();
-//         for(int i=0;i<n-1;i++){
-//             for(int j=0;j<n;j++){
-//                 if(nums[i] == nums[j]){
-//                     return true;
-//                 }
-//             }
-//         }
-//         return false;
-//     }
-// };
-
-
-// unordered_map<int,int> mpp;
-//         int n = nums.size()-1;
-//         for(int i=0;i<n;i++){
-//             mpp[nums[i]]++;
-//         }
-//         for(int i=0;i<n;i++){
-//             if(mpp[nums[i]] == 1){
-//                 return false;
-//             }
-//         }
-//         return true;
