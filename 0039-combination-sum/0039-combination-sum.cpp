e5a3e95f56d8f32b81a0aa713e0b76edbd95a3d1
Devanshi Gupta <121246424+DevanshiGupta44@users.noreply.github.com>
2024-06-11 17:59:57 +0530

    Time: 8 ms (47.21%), Space: 12.3 MB (85.06%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,29 @@
 class Solution {
-public:
-    void findcombo(int ind, int target, vector<int>& arr, vector<vector<int>>& ans,vector<int>& ds){
-        if(ind == arr.size()){
-            if(target==0){
-                ans.push_back(ds);
-            }
-            return ;
+  public:
+    void findcombo(int ind, int target,vector<int>& arr,vector<vector<int>>& ans,vector<int>& ds){
+    if(ind == arr.size()){
+        if(target==0){
+            ans.push_back(ds);
         }
-        
-        if(arr[ind]<=target){
-            ds.push_back(arr[ind]);
-            findcombo(ind,target-arr[ind],arr,ans,ds);
-            ds.pop_back();
-        }
-        
-        findcombo(ind+1,target,arr,ans,ds);
+        return;
     }
-public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    
+    if(arr[ind]<=target){
+        ds.push_back(arr[ind]);
+        findcombo(ind,target-arr[ind],arr,ans,ds);
+        ds.pop_back();
+    }
+    
+    findcombo(ind+1,target,arr,ans,ds);
+}
+  public:
+    //Function to return a list of indexes denoting the required 
+    //combinations whose sum is equal to given number.
+    vector<vector<int>> combinationSum(vector<int> & combinations, int target) {
+        // Your code here
         vector<vector<int>> ans;
         vector<int> ds;
-        findcombo(0,target,candidates,ans,ds);
+        findcombo(0,target,combinations,ans,ds);
         return ans;
     }
 };
-                      
