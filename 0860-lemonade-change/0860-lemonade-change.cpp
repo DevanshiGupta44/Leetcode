d20e46cad724a856b861994859c96789f39bf6ed
Devanshi Gupta <121246424+DevanshiGupta44@users.noreply.github.com>
2024-08-08 11:12:02 +0530

    Time: 72 ms (58.19%), Space: 86.1 MB (27.46%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five=0;
+        int ten=0;
+        int n = bills.size();
+        for(int i=0;i<n;i++){
+            if(bills[i] == 5){
+                five++;
+            }
+            else if(bills[i] == 10){
+                ten++;
+                if(five){
+                    five--;
+                }
+                else{
+                    return false;
+                }
+            }
+            else if(bills[i] == 20){
+                if(ten>0 && five>0){
+                    ten--;
+                    five--;
+                }
+                else if(five>=3){
+                    five=five-3;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
