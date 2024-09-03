bdf9d5dd984151d07813b6c6a51c7ba3bd33e778
Devanshi Gupta <121246424+DevanshiGupta44@users.noreply.github.com>
2024-09-03 18:43:33 +0530

    Time: 117 ms (82.45%), Space: 112.8 MB (7.89%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -4,21 +4,14 @@ public:
         int n = nums.size();
         int sum = 0;
         double maxi = INT_MIN;
-
-        // Calculate the sum of the first k elements
         for (int i = 0; i < k; i++) {
             sum += nums[i];
         }
-
-        // Initialize maxi with the average of the first k elements
         maxi = static_cast<double>(sum) / k;
-
-        // Sliding window: Move the window across the array
         for (int i = k; i < n; i++) {
             sum += nums[i] - nums[i - k];
             maxi = max(maxi, static_cast<double>(sum) / k);
         }
-
         return maxi;
     }
 };
